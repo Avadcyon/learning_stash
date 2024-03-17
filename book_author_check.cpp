f5ce338488c37fd24cb6b0d9c8b62b5d6b858a50
Svyatozar <142081136+Avadcyon@users.noreply.github.com>
2024-03-18 03:23:37 +1000

    Update book_author_check.cpp

--- book_author_check.cpp
+++ book_author_check.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <set>
 using namespace std;
 struct Book {
 	string name;
 	string date;
 	string author;
 };
-void condition_check(Book* info, string our_author) {
-	for (int i; i < 3; i++) {
-		if (info->date == "1980" || info->date == "1983" || info->date == "1982" || info->date == "1983" || info->date == "1984" || info->date == "1985") {
+void condition_check(Book* info, string our_author,int count_of_books) {
+	set<string> valid_dates = { "1980","1981","1982","1983","1984","1985" };
+	for (int i; i < count_of_books; i++) {
+		if (valid_dates.count(info->date) > 0) {
 			if (info->author == our_author) {
 				cout << info->name;
 			}
@@ -24,5 +26,5 @@ int main() {
 	Book *books = new Book[size];
 	cout << "Введите имя автора";
 	cin >> author_name;
-	condition_check(books,author_name);
+	condition_check(books,author_name,size);
 }
